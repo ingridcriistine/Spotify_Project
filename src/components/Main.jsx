164eacd4600552ff3d2d60232d93a2c9861326ca
ingridcriistine <ingridrochacristine@gmail.com>
2025-02-11 22:47:36 -0300

    updates

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,26 +1,13 @@
 import React from "react";
-import FontAwesomeIcon from "@fontawesome/react-fontawesome"
-import faCirclePlay from "@fontawesome/free-solid-svg-icons"
+import ItemList from "./ItemList";
+import { artistArray } from "../assets/database/artists";
+import { songsArray } from "../assets/database/songs";
 
 const Main = () => {
     return (
         <div className="main ">
-            <div className="item-list">
-                <div className="item-list__header">
-
-                </div>
-
-                <div className="single-item">
-                    <img className="single-item__image" src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4" alt="Imagem do artista X"/>
-                    
-                </div>
-                
-                <FontAwesomeIcon className="single-item__icon" icon={faCirclePlay}/>
-            </div>
-            <div className="single-item__texts">
-                <p className="single-item__title">Henrique e Juliano</p>
-                <p className="single-item__type">Artista</p>
-            </div>
+            <ItemList title="Artistas" items={5} itemsArray={artistArray} path="/artists" idPath="/artist"/>
+            <ItemList title="Músicas" items={10} itemsArray={songsArray} path="/songs" idPath="/song"/>
         </div>
     )
 }
---
+++ src/components/SingleItem.jsx
@@ -0,0 +1,24 @@
+import React from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faCirclePlay } from "@fortawesome/free-solid-svg-icons";
+
+const SingleItem = ({ id, name, image, banner, artist = undefined, idPath }) => {
+    return (
+        <a href={`${idPath}/${id}`} className="single-item">
+            <div className="single-item__div-image-button">
+                <div className="single-item__div-image">
+                    <img className="single-item__image" src={image} alt={`Imagem do artista {$name}`} />
+                </div>
+                <FontAwesomeIcon className="single-item__icon" icon={faCirclePlay} />
+            </div>
+            <div className="single-item__texts">
+                <div className="single-item__2lines">
+                    <p className="single-item__title">{name}</p>
+                </div>
+                <p className="single-item__type">{artist ?? "Artista"}</p>
+            </div>
+        </a>
+    )
+}
+
+export default SingleItem
