c32621f85b859a5bf610e2b9aedc6ae2bdaadea4
ingridcriistine <ingridrochacristine@gmail.com>
2025-02-13 22:41:49 -0300

    updates

--- src/components/SongItem.jsx
+++ src/components/SongItem.jsx
@@ -1,19 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const SongItem = () => {
-    return (
-        <Link to="/song/1" className="song-item">
-            <div className="song-item__number-album">
-                <p>1</p>
-                <div className="song-item__album">
-                    <img src="" alt="Imagem da música X" className="song-item__image"/>
-                    <p className="song-item__name">Nome música</p>
-                </div>
-            </div>
-            <p className="song-item__name">02:20</p>
-        </Link>
-    )
-}
+const SongItem = ({ image, name, duration, artist, audio, id, index }) => {
+  return (
+    <Link to={`/song/${id}`} className="song-item">
+      <div className="song-item__number-album">
+        <p>{index + 1}</p>
+
+        <div className="song-item__album">
+          <img
+            src={image}
+            alt={`Imagem da Música ${name}`}
+            className="song-item__image"
+          />
+
+          <p className="song-item__name">{name}</p>
+        </div>
+      </div>
+
+      <p>{duration}</p>
+    </Link>
+  );
+};
 
 export default SongItem;
